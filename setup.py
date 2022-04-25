94331f84a8da7890cb6fdbb148e84b183019a5c1
mathvdd <mathvdd@gmail.com>
2022-04-25 12:40:37 +0200

    added setup.py for installation from github

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 25 12:31:42 2022
+
+@author: Mathieu Vander Donckt
+mathieu.vanderdonckt@uliege.be
+
+see https://github.com/MichaelKim0407/tutorial-pip-package
+"""
+
+from setuptools import setup
+
+setup(
+    name='TRAPPIST-utils',
+    version='dev',
+
+    url='https://github.com/mathvdd/TRAPPIST-utils',
+    author='Mathieu Vander Donckt',
+    author_email='mathieu.vanderdonckt@uliege.be',
+
+    packages=['trap_reduction', 'trap_plot', 'query_NAS', 'get_ephem'],
+)
