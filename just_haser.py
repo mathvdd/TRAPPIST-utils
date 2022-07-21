b73738cbfb1fa29318e14808cf82fe18284f5a52
mathvdd <mathieu@mathvdd.com>
2022-07-21 10:56:30 +0200

    cleaned just_haser

--- just_haser.py
+++ just_haser.py
@@ -15,39 +15,39 @@ comet = 'CK19L030'
 night = '2022-01-08'
 obs = 'TN'
 
-reduced_dir = os.path.join(param['reduced'], comet, night.replace('-','') + obs)
-profiles_dir = os.path.join(reduced_dir, "profiles")
-haser_dir = os.path.join(reduced_dir, "haser")
-garbage_dir = os.path.join(reduced_dir, "probably_garbage")
-
-if os.path.exists(param['tmpout']):
-    shutil.rmtree(param['tmpout'])
-os.mkdir(param['tmpout'])
-
-print('--- copying files ---')
-for path, subdirs, files in os.walk(profiles_dir):
-    for file in files:
-        shutil.copy(os.path.join(path, file), os.path.join(param['tmpout'], file))
-shutil.copy(os.path.join(haser_dir, 'inputhaser-BC'), os.path.join(param['tmpout'], 'inputhaser-BC'))
-shutil.copy(os.path.join(garbage_dir, 'ephem.brol'), os.path.join(param['tmpout'], 'ephem.brol'))
-
-conda = True if param['conda'] == 'True' else False #wether to use 'source activate to launch cl or not'
-print('--- launching hasercalctest ---')
-while True:
-    trap_reduction.clhasercalctest(param['iraf'], 'yes', conda=conda)
-    trap_plot.plot_haserprofile(param['tmpout'],comet_name=comet)
+def haser_import_1night(comet, night, obs):
+    reduced_dir = os.path.join(param['reduced'], comet, night.replace('-','') + obs)
+    profiles_dir = os.path.join(reduced_dir, "profiles")
+    haser_dir = os.path.join(reduced_dir, "haser")
+    garbage_dir = os.path.join(reduced_dir, "probably_garbage")
+    print(f'Initiating haser for {comet}, {night}, {obs}')
+    if os.path.exists(param['tmpout']):
+        shutil.rmtree(param['tmpout'])
+    os.mkdir(param['tmpout'])
+    # print('--- copying files ---')
+    for path, subdirs, files in os.walk(profiles_dir):
+        for file in files:
+            shutil.copy(os.path.join(path, file), os.path.join(param['tmpout'], file))
+    shutil.copy(os.path.join(haser_dir, 'inputhaser-BC'), os.path.join(param['tmpout'], 'inputhaser-BC'))
+    shutil.copy(os.path.join(garbage_dir, 'ephem.brol'), os.path.join(param['tmpout'], 'ephem.brol'))
+    
+    conda = True if param['conda'] == 'True' else False #wether to use 'source activate to launch cl or not'
+    print('--- launching hasercalctest ---')
     while True:
-        inp = input('relaunch hasercalctext (r) or overwrite results in reduced dir (c)? [r/c]')
-        if inp == 'r' or inp == 'R' or inp == 'c' or inp == 'C':
+        trap_reduction.clhasercalctest(param['iraf'], 'yes', conda=conda)
+        trap_plot.plot_haserprofile(param['tmpout'],comet_name=comet)
+        while True:
+            inp = input('relaunch hasercalctext (r) or overwrite results in reduced dir (c)? [r/c]')
+            if inp == 'r' or inp == 'R' or inp == 'c' or inp == 'C':
+                break
+        if inp == 'r' or inp == 'R':
+            print('relaunching hasercalctest')
+            continue
+        elif inp == 'c' or inp == 'C':
+            print('overwriting results')
+            for path2, subdirs2, files2 in os.walk(param['tmpout']):
+                for file in files2:
+                    if 'haser' in file and 'tmp' not in file:
+                        shutil.copy(os.path.join(path2, file), os.path.join(haser_dir, file))
+                        print('copied', file, "in reduced dir")
             break
-    if inp == 'r' or inp == 'R':
-        print('relaunching hasercalctest')
-        continue
-    elif inp == 'c' or inp == 'C':
-        print('overwriting results')
-        for path2, subdirs2, files2 in os.walk(param['tmpout']):
-            for file in files2:
-                if 'haser' in file and 'tmp' not in file:
-                    shutil.copy(os.path.join(path2, file), os.path.join(haser_dir, file))
-                    print('copied', file, "in reduced dir")
-        break
--- trappist_init_weekly.py
+++ trappist_init_weekly.py
@@ -20,10 +20,10 @@ import phase_angle
 
 ########################
 # INPUT PARAMETERS
-startdate = '2022-06-25' #the night starting
+startdate = '2016-06-25' #the night starting
 enddate = '2022-07-15' #starting that night not included
 obs = 'TS'
-comets = [] # list of comets to take into account. set empty to take all 
+comets = ['CK17K020'] # list of comets to take into account. set empty to take all 
 skip = True # skip raw data directory donwload if data already in raw_data.
 # skip reduction if there is already a set of reduced data
 # If set to False, will ask what to do in both cases
